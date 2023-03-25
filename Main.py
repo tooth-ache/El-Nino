30d5994be4c3f88461c2532091f4e8efe4c1e04f
Patryk Drozd <68166254+drozd324@users.noreply.github.com>
2023-03-25 13:47:05 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
+# declaring constants
+
 A = 1.0
 B = 663.0
 C = 3.0
@@ -9,31 +11,65 @@ T_star = 12.0
 u_star = -14.2
 delta_x = 7.5
 
-def u(state, t):
-    u, T_e, T_w = state
-    return B / delta_x * (T_e - T_w) - C * (u - u_star)
+# making the vectorised differential equations
 
 def f(state, t):
     u, T_e, T_w = state
-    return [  B / delta_x * (T_e - T_w) - C * (u - u_star),
-            u * T_w / (2 * delta_x) - A * (T_e - T_star),
-            -u * T_e / (2 * delta_x) - A * (T_w - T_star) ]
+
+    # setting up the 3 differential equations
+
+    du = B / delta_x * (T_e - T_w) - C * (u - u_star) #  = du/dt
+    dTe = u * T_w / (2 * delta_x) - A * (T_e - T_star) #  = dTe/dt
+    dTw = -u * T_e / (2 * delta_x) - A * (T_w - T_star) #  = dTw/dt
+
+    return du, dTe, dTw 
 
 
-t = np.arange(0, 20, .01)
-state_0 = [10, 10, 14]
+dt = .01
+t_start = 0
+t_end = 20
+
+t = np.arange(t_start, t_end, dt)
+state_0 = [10, 10, 14] # initial conditions
+y = odeint(f, state_0, t) # y is a list with elemets that are list with 3 entries containing u, Te, Tw for each time step
+
+# this corresponds to the functions with respect to time
+u = y[:,0]
+Te = y[:,1] 
+Tw = y[:,2]
+
+# first plots
+
+plt.plot(t, u)
+plt.title("Current velocity against time ({} years)".format(t_end))
+plt.xlabel("Time t (years)")
+plt.ylabel("Current Velocity u ") # 10^3km / year  units?
+plt.ylim((-400,400))
+plt.show()
+
+plt.plot(t, Te - Tw)
+plt.title("Difference in Temp against time ({} years)".format(t_end))
+plt.xlabel("Time t (years)")
+plt.ylabel("T_e - T_w ")
+plt.ylim((-30,30))
+plt.show()
+
+# renaming variables to integrating for longer times
+
+dt = .01
+t_start = 0
+t_end = 1000
+t = np.arange(t_start, t_end, dt)
+state_0 = [10, 10, 14] 
 y = odeint(f, state_0, t)
 
-u = []
-for i in range(len(y)):
-    u.append(y[i][0])
+u = y[:,0]
+Te = y[:,1] 
+Tw = y[:,2]
 
-dT = []
-for i in range(len(y)):
-    dT.append(y[i][1] - y[i][2])
-
-#plt.plot(t, u)
-#plt.plot(t, dT)
-plt.plot(t, y[:, 0])
-#plt.ylim((-400,400))
+plt.plot(t, u)
+plt.title("Current velocity against time ({} years)".format(t_end))
+plt.xlabel("Time t (years)")
+plt.ylabel("Current Velocity u ") # 10^3km / year  units?
+plt.ylim((-400,400))
 plt.show()
